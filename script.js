0b7dae30e312b28082364e775ac4ea5d366b9fc5
Thijsvandersandt <140083145+Thijsvandersandt@users.noreply.github.com>
2023-07-20 15:42:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript code for slideshow functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change slide every 2 seconds (adjust as per your requirement)
+}
